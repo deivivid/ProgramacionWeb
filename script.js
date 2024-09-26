3ed7a993e2b1307790e937c481d6c222aebd6669
Deivivid <160372646+deivivid@users.noreply.github.com>
2024-09-26 15:16:59 -0600

    Creacion de directorio y archivo

---
+++ script.js
@@ -0,0 +1,29 @@
+//CLASE 24/09/24
+//NOMBRE: RIVAS GARZA DAVID
+
+/*
+Variables:
+*/
+
+let nombre = "Juan";
+const PI = 3.1416;
+var edad = 25;
+
+alert('Hola, clase de 5IV7 desde el JS externo');
+
+//operadores
+
+let a = 5;
+let b = 3;
+let suma = a+b;
+let resta = a-b;
+let multiplicacion = a*b;
+let division = a/b;
+
+//operadores de comparación
+
+a>b; //T
+a<b; //F
+a == b; //F
+a ===b; //F
+
